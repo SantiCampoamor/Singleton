d7d173e0155138a08dba4167a7ebc9d854224fc2
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-10-13 10:38:59 -0300

    Initial commit

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] string[] sceneNames;
+    [SerializeField] int sceneIndex;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ChangeSceneSequentially()
+    {
+        if (sceneIndex < sceneNames.Length)
+        {
+            sceneIndex = 0;
+        }
+        ChangeSceneAndIncrementIndex(); 
+    }
+    void ChangeSceneAndIncrementIndex()
+    {
+        SceneManager.LoadScene(sceneNames[sceneIndex]);
+        sceneIndex++;
+    }
+}
